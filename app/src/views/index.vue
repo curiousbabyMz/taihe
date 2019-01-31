0c8c066b668b92b0f1a3bb57e30217dc07300088
Mz <285532050@qq.com>
2019-02-01 01:22:43 +0800

    home edit

---
+++ app/src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <page />
+  </div>
+</template>
+<style>
+.container {
+  height: inherit;
+}
+</style>
+
+<script>
+import Page from "@/components/page.vue";
+export default {
+  name: "Index",
+  components: {
+    page: () => import("@/components/page.vue")
+  }
+};
+</script>
